33b7b7d8cf26db05c354d5fc48d3d7122d329d7a
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:35:21 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/notifications/slack/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for Slack notifications.",
+    categories = PluginSubGroup.PluginCategory.ALERTING
+)
+package io.kestra.plugin.notifications.slack;
+
+import io.kestra.core.models.annotations.PluginSubGroup;

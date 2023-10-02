4ef1896a8d72b0ca3035b9da16286fadaf3f71f9
iNikitaGricenko <VfVristalt@gmail.com>
2023-10-02 15:50:35 +0300

    add WhatsApp webhook notification

---
+++ src/main/java/io/kestra/plugin/notifications/whatsapp/WhatsAppIncomingWebhook.java
@@ -0,0 +1,110 @@
+package io.kestra.plugin.notifications.whatsapp;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.models.tasks.Task;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.runners.RunContext;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.netty.DefaultHttpClient;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import java.net.URI;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Send a What's app message using an Incoming Webhook",
+    description = "Add this task to a list of `errors` tasks to implement custom flow-level failure notifications. Check the <a href=\"https://developers.facebook.com/docs/whatsapp/on-premises/guides/webhooks\">WhatsApp documentation</a> for more details.."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Send a WhatsApp notification on a failed flow execution",
+            full = true,
+            code = """
+                id: unreliable_flow
+                namespace: prod
+
+                tasks:
+                  - id: fail
+                    type: io.kestra.plugin.scripts.shell.Commands
+                    runner: PROCESS
+                    commands:
+                      - exit 1
+
+                errors:
+                  - id: alert_on_failure
+                    type: io.kestra.plugin.notifications.whatsapp.WhatsAppIncomingWebhook
+                    url: "{{ secret('WHATSAPP_WEBHOOK') }}" # https://webhook.your-domain
+                    payload: |
+                      {
+                        "username": "MyUsername",
+                        "title": "What's app Alert",
+                        "content": "Failure alert for flow {{ flow.namespace }}.{{ flow.id }} with ID {{ execution.id }}"
+                      }
+                """
+        ),
+        @Example(
+            title = "Send a WhatsApp message via incoming webhook",
+            full = true,
+            code = """
+                id: whatsapp_incoming_webhook
+                namespace: dev
+
+                tasks:
+                  - id: send_whatsapp_message
+                    type: io.kestra.plugin.notifications.whatsapp.WhatsAppIncomingWebhook
+                    url: "{{ secret('WHATSAPP_WEBHOOK') }}"
+                    payload: |
+                      {
+                        "username": "MyUsername",
+                        "title": "WhatsApp Hello",
+                        "content": "Hello from the workflow {{ flow.id }}",
+                        "color"[255, 255, 255]
+                      }            
+                """
+        ),
+    }
+)
+public class WhatsAppIncomingWebhook extends Task implements RunnableTask<VoidOutput> {
+
+    @Schema(
+        title = "Webhook URL which should be taken from whatsapp integrations tab"
+    )
+    @PluginProperty(dynamic = true)
+    @NotBlank
+    protected String url;
+
+    @Schema(
+        title = "What's app message payload"
+    )
+    @PluginProperty(dynamic = true)
+    protected String payload;
+
+    @Override
+    public VoidOutput run(RunContext runContext) throws Exception {
+        String url = runContext.render(this.url);
+
+        try (DefaultHttpClient client = new DefaultHttpClient(URI.create(url))) {
+            String payload = runContext.render(this.payload);
+
+            runContext.logger().debug("Send What's app webhook: {}", payload);
+
+            client.toBlocking().retrieve(HttpRequest.POST(url, payload));
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/io/kestra/plugin/notifications/whatsapp/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for What's App notifications.",
+    categories = PluginSubGroup.PluginCategory.ALERTING
+)
+package io.kestra.plugin.notifications.whatsapp;
+
+import io.kestra.core.models.annotations.PluginSubGroup;

944c101b207c0a8694dad724534e189b1a41b590
iNikitaGricenko <VfVristalt@gmail.com>
2023-10-04 10:59:31 +0300

    Add Zenduty alert plugin

---
+++ src/test/java/io/kestra/plugin/notifications/zenduty/ZendutyExecutionTest.java
@@ -0,0 +1,65 @@
+package io.kestra.plugin.notifications.zenduty;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.repositories.LocalFlowRepositoryLoader;
+import io.kestra.core.runners.RunnerUtils;
+import io.kestra.plugin.notifications.FakeWebhookController;
+import io.kestra.runner.memory.MemoryRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+
+@MicronautTest
+public class ZendutyExecutionTest {
+
+    @Inject
+    private ApplicationContext applicationContext;
+
+    @Inject
+    protected MemoryRunner runner;
+
+    @Inject
+    protected RunnerUtils runnerUtils;
+
+    @Inject
+    protected LocalFlowRepositoryLoader repositoryLoader;
+
+    @BeforeEach
+    protected void init() throws IOException, URISyntaxException {
+        repositoryLoader.load(Objects.requireNonNull(ZendutyExecutionTest.class.getClassLoader().getResource("flows")));
+        this.runner.run();
+    }
+
+    @Test
+    void flow() throws TimeoutException {
+        EmbeddedServer embeddedServer = applicationContext.getBean(EmbeddedServer.class);
+        embeddedServer.start();
+
+        Execution execution = runnerUtils.runOne(
+            "io.kestra.tests",
+            "zenduty",
+            null,
+            (f, e) -> ImmutableMap.of("url", embeddedServer.getURI().toString())
+        );
+
+        assertThat(execution.getTaskRunList(), hasSize(3));
+        assertThat(FakeWebhookController.data, containsString(execution.getId()));
+        assertThat(FakeWebhookController.data, containsString("https://mysuperhost.com/kestra/ui"));
+        assertThat(FakeWebhookController.data, containsString("Failed on task `failed`"));
+        assertThat(FakeWebhookController.data, containsString("Kestra Zenduty alert"));
+    }
+
+}

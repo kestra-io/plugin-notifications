4604f6f139c3e13452638cafe839336016cb5910
Loïc Mathieu <loikeseke@gmail.com>
2025-10-13 14:52:45 +0200

    fix: there can be no taskrun in an execution
    Fixes https://github.com/kestra-io/kestra/issues/11890

--- src/test/java/io/kestra/plugin/notifications/service/ExecutionServiceTest.java
+++ src/test/java/io/kestra/plugin/notifications/service/ExecutionServiceTest.java
@@ -1,15 +1,36 @@
 package io.kestra.plugin.notifications.service;
 
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.flows.Flow;
 import io.kestra.core.models.flows.State;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.repositories.ExecutionRepositoryInterface;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.tenant.TenantService;
+import io.kestra.plugin.core.log.Log;
+import io.kestra.plugin.notifications.ExecutionInterface;
+import io.kestra.plugin.notifications.services.ExecutionService;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static io.kestra.plugin.notifications.services.ExecutionService.isExecutionInTheWantedState;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@KestraTest
 class ExecutionServiceTest {
+    @Inject
+    private RunContextFactory runContextFactory;
+
+    @Inject
+    private ExecutionRepositoryInterface executionRepository;
+
     // same execution
     @Test
     void isExecutionInTheWantedState_sameExecution() {
@@ -61,5 +82,43 @@ class ExecutionServiceTest {
                 false,
                 Optional.of(State.Type.PAUSED.name()))
         ).isFalse();
+    }
+
+    @Test
+    void executionMapShouldNotFailWhenNoTaskrun() throws IllegalVariableEvaluationException {
+        var flow = Flow.builder()
+            .tenantId(TenantService.MAIN_TENANT)
+            .namespace("namespace")
+            .id("flow")
+            .tasks(List.of(Log.builder().id("log").message("log message").build()))
+            .build();
+        var execution = Execution.newExecution(flow, Collections.emptyList())
+            .withState(State.Type.FAILED);
+        executionRepository.save(execution);
+        var runContext = runContextFactory.of(flow, execution);
+
+        var executionMap = ExecutionService.executionMap(runContext, new ExecutionInterface() {
+            @Override
+            public Property<String> getExecutionId() {
+                return Property.ofValue(execution.getId());
+            }
+
+            @Override
+            public Property<Map<String, Object>> getCustomFields() {
+                return null;
+            }
+
+            @Override
+            public Property<String> getCustomMessage() {
+                return null;
+            }
+        });
+
+        assertThat(executionMap).isNotNull();
+        assertThat(executionMap).hasSize(4);
+        assertThat(executionMap).containsKey("duration");
+        assertThat(executionMap).containsKey("execution");
+        assertThat(executionMap).containsKey("link");
+        assertThat(executionMap).containsKey("startDate");
     }
 }
